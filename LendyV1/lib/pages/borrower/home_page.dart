691497fdd26f83afcebd4bf5b3fb7c458514d50c
ademulyana <100661834+adem299@users.noreply.github.com>
2023-05-22 11:33:58 +0700

    Add files via upload

---
+++ LendyV1/lib/pages/borrower/home_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lendyv1/pages/borrower/profile_pages.dart';
+import 'package:lendyv1/pages/borrower/main/pengajuan_page.dart';
+import 'package:lendyv1/pages/borrower/profile_pages.dart';
+import 'package:lendyv1/util/Color/ColorCustom.dart';
+import 'my_home_page.dart';
+import 'activity_page.dart';
+import 'notification_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _pages = <Widget>[
+    MyHomePage(),
+    NotificationPage(),
+    PengajuanPage(),
+    ActivityPage(),
+    ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: true,
+        // backgroundColor: AppColors.bgColor,
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications_rounded),
+              label: 'Notification',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.handshake_rounded),
+              label: 'Ajukan',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart_rounded),
+              label: 'Activity',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_rounded),
+              label: 'Profile',
+            ),
+          ],
+          iconSize: 30,
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color.fromARGB(255, 4, 196, 199),
+          unselectedItemColor: Colors.grey[500],
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          backgroundColor: AppColors.mainColor,
+          onTap: _onItemTapped,
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff001523),
+                Color(0xFF006466),
+              ],
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              stops: [0.4, 0.7],
+              tileMode: TileMode.repeated,
+            ),
+          ),
+          child: _pages.elementAt(_selectedIndex),
+        ));
+  }
+}
